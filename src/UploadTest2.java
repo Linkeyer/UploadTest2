835f6258d42ed6777046d1dbbcde54690b35c0e6
Linkeyer <461704233@qq.com>
2017-11-15 00:24:09 +0800

    Changed for the first time

--- src/UploadTest2.java
+++ src/UploadTest2.java
@@ -1,5 +1,6 @@
 public class UploadTest2{
 	public static void main(String[] args){
 		System.out.println("This is the second Upload Test!");
+		System.out.println("This is another Upload!");
 	}
 }
